4b9b91987f18ba8d22a955aaf3ec0ae725d0368e
VedanthM <65438508+Vedanth29@users.noreply.github.com>
2022-07-15 16:08:01 +0530

    Create Demo.java

---
+++ Class and Object/Demo.java
@@ -0,0 +1,16 @@
+public class Demo
+{
+    int a =20; String b = "Vedanth";
+    void print()
+    {
+        System.out.print(a+" "+b);
+    }
+}
+class Test
+{
+    public static void main(String[] args)
+    {
+        Demo r = new Demo();
+        r.print();
+    }
+}
